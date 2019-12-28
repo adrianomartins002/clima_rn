03e1642bd49d43982d0e5998cb0f4e2f7d6f70fb
Adriano Martins <adrianomartins002@gmail.com>
2019-12-28 15:22:15 -0300

    feat(iniciar): iniciar o projeto

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Iniciando!</Text>
+    </View>
+  );
+};
+
+export default App;
