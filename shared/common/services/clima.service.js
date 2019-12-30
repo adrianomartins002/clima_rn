d4ee28a1deeb118c763b207ab1b5cf5ede69c3f7
Adriano Martins <adrianomartins002@gmail.com>
2019-12-30 01:02:24 -0300

    gitflow-feature-stash: localizacao

--- shared/common/services/clima.service.js
+++ shared/common/services/clima.service.js
@@ -3,13 +3,35 @@ import {executarServico} from '../../common/utils/api';
 import {informacoesPorCodigo} from '../../common/utils/consts';
 
 export const ClimaService = {
+  /**
+   * @method metodo responsável por retornar objeto com as informações que serão utilizadas na página
+   * @param {*} result parametro com o resultado vindo da api de localizacao
+   *
+   */
+
   async recuperarClimaPelaLocalizacao(latitude, longitude) {
     const {data} = await executarServico(
       api.recuperarClima(latitude, longitude),
     );
-    return data;
+    return {
+      endereco: data.name,
+      temperatura: parseInt(data.main.temp),
+      sensacao: data.main.feels_like,
+      vento: data.wind.speed,
+      nuvens: data.clouds.all,
+      pressao: data.main.pressure,
+      pais: data.sys.country,
+      color: this.tratarTemaPorCodigo(data.weather[0].icon)[0].cor,
+      icone: this.tratarTemaPorCodigo(data.weather[0].icon)[0].icone,
+    };
   },
 
+  /**
+   *
+   * @param {*} codigo código vem a partir do resultado ad request da api do openweather
+   * Ele indica imagem relacionada ao tempo da localização
+   * para verificar os possíveis códigos é só acessar o link: https://openweathermap.org/weather-conditions
+   */
   tratarTemaPorCodigo(codigo) {
     return informacoesPorCodigo.filter(
       informacao => informacao.codigo === codigo,
