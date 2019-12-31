17e4d6ec8beb5da753e274ebe633fd400c6fa465
Adriano Martins <adrianomartins002@gmail.com>
2019-12-30 22:56:09 -0300

    feat(loading): melhorar loading

--- shared/components/molecules/loading.component.js
+++ shared/components/molecules/loading.component.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ActivityIndicator,
-  StatusBar,
-} from 'react-native';
-import Sun from '../../common/assets/illustrations/sun-glass.svg';
+import {View, StyleSheet} from 'react-native';
+import AnimatedLoader from 'react-native-animated-loader';
 
 export const LoadingComponent = props => {
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      {/* <StatusBar backgroundColor="white" barStyle="dark-content" /> */}
       <View style={styles.containerTemp} />
-      <Sun width={180} height={110} />
-      <View style={styles.containerCarregando}>
-        <Text style={styles.textCarregando}>Carregando</Text>
-        <ActivityIndicator size="large" color="#0090C7" />
-      </View>
+      <View style={styles.containerCarregando} />
+      <AnimatedLoader
+        visible={true}
+        overlayColor="rgba(255,255,255,0.75)"
+        source={require('../../common/assets/animations/loader-sun2.json')}
+        animationStyle={[
+          styles.containerAnimation,
+          {
+            backgroundColor: props.backgroundColor,
+          },
+        ]}
+        speed={1}
+      />
     </View>
   );
 };
@@ -27,6 +29,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  containerAnimation: {
+    width: '100%',
+    height: '100%',
+    marginLeft: 10,
+    opacity: 1,
+    backgroundColor: '#FFF',
   },
   containerTemp: {
     flex: 1,
